2d26de9b95bd863f34082cb51b09fb76605c03fb
Kevin Huang <kevinhuang492@gmail.com>
2019-07-17 11:52:44 -0700

    init

---
+++ app/src/main/java/com/kevinhuang/androiidfinalproject/MainActivity.java
@@ -0,0 +1,17 @@
+package com.kevinhuang.androiidfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mStartButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
