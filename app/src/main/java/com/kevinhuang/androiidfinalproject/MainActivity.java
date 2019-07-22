400dc70691e6005084cae4c066852252f63656c0
Kevin Huang <kevinhuang492@gmail.com>
2019-07-22 11:47:40 -0700

    init

--- app/src/main/java/com/kevinhuang/androiidfinalproject/MainActivity.java
+++ app/src/main/java/com/kevinhuang/androiidfinalproject/MainActivity.java
@@ -3,15 +3,28 @@ package com.kevinhuang.androiidfinalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button mStartButton;
-
+    ImageView mBellIcon;
+    Animation fromBottom;
+    Animation fromTop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStartButton = (Button) findViewById(R.id.start_button);
+        mBellIcon = (ImageView) findViewById(R.id.bell_icon);
+
+        fromBottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
+        fromTop = AnimationUtils.loadAnimation(this,R.anim.fromtop);
+        mStartButton.setAnimation(fromBottom);
+        mBellIcon.setAnimation(fromTop);
     }
 }
