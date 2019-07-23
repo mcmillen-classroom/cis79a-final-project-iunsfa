40a6f95bc138f2cb1d24bcd6ced8ee392bb3a867
Kevin Huang <kevinhuang492@gmail.com>
2019-07-23 12:52:56 -0700

    init

--- app/src/main/java/com/kevinhuang/androiidfinalproject/MainActivity.java
+++ app/src/main/java/com/kevinhuang/androiidfinalproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.kevinhuang.androiidfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -10,21 +12,41 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button mStartButton;
-    ImageView mBellIcon;
-    Animation fromBottom;
-    Animation fromTop;
+    private Button mStartButton;
+    private ImageView mBellIcon;
+    private Animation fromBottom;
+    private Animation fromTop;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mStartButton = (Button) findViewById(R.id.start_button);
-        mBellIcon = (ImageView) findViewById(R.id.bell_icon);
 
-        fromBottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
-        fromTop = AnimationUtils.loadAnimation(this,R.anim.fromtop);
+        mStartButton = (Button) findViewById(R.id.start_button);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openScreen();
+            }
+        });
+
+
+        mBellIcon = (ImageView) findViewById(R.id.bell_icon);
+        fromBottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
+        fromTop = AnimationUtils.loadAnimation(this, R.anim.fromtop);
         mStartButton.setAnimation(fromBottom);
         mBellIcon.setAnimation(fromTop);
     }
+
+    public void openScreen() {
+
+        Intent intent = new Intent(this, Screen2.class);
+        startActivity(intent);
+
+
+    }
 }
+
+
--- app/src/main/java/com/kevinhuang/androiidfinalproject/Screen2.java
+++ app/src/main/java/com/kevinhuang/androiidfinalproject/Screen2.java
@@ -2,13 +2,33 @@ package com.kevinhuang.androiidfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Screen2 extends AppCompatActivity {
+    private Button mSetupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen2);
+        mSetupButton = (Button) findViewById(R.id.setup_button);
+        mSetupButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                openScreen2();
+            }
+        });
+
+    }
+    public void openScreen2() {
+
+        Intent intent = new Intent(this, Screen3.class);
+        startActivity(intent);
+
+
     }
 }
