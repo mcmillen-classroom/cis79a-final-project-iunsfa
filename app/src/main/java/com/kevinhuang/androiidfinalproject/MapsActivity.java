f8c4d31ba8fd3485e8fe92fb43ebd59428cb9ce7
iunsfa <kevinhuang492@gmail.com>
2019-07-23 20:40:57 -0700

    final commit

---
+++ app/src/main/java/com/kevinhuang/androiidfinalproject/MapsActivity.java
@@ -0,0 +1,111 @@
+package com.kevinhuang.androiidfinalproject;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    LocationManager locationManager;
+    LocationListener locationListener;
+    LatLng userLatLng;
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                //stores user latlng
+                userLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.clear(); //clear old location marker in google map
+                mMap.addMarker(new MarkerOptions().position(userLatLng).title("Your Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(userLatLng));
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        //asking for permission with user/location permission
+        askLocationPermission();
+
+
+    }
+
+    private void askLocationPermission() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                if (ActivityCompat.checkSelfPermission(getBaseContext(),Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getBaseContext(),Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                    return;
+                }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+                //getting user last location to set the default location marker in the map
+
+                Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                userLatLng = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                mMap.clear(); //clear old location marker in google map
+                mMap.addMarker(new MarkerOptions().position(userLatLng).title("Your Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(userLatLng));
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                token.continuePermissionRequest();
+            }
+        }).check();
+    }
+}
--- app/src/main/java/com/kevinhuang/androiidfinalproject/Screen3.java
+++ app/src/main/java/com/kevinhuang/androiidfinalproject/Screen3.java
@@ -2,13 +2,56 @@ package com.kevinhuang.androiidfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Screen3 extends AppCompatActivity {
-
+public class Screen3 extends AppCompatActivity implements View.OnClickListener {
+    private Button mSlot1;
+    private Button mSlot2;
+    private Button mSlot3;
+    private Button mSlot4;
+    private Button mSlot5;
+    private Button mSlot6;
+    private Button mSlot7;
+    private Button mSlot8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen3);
+        mSlot1 = (Button) findViewById(R.id.slot_1);
+        mSlot2 = (Button) findViewById(R.id.slot_2);
+        mSlot3 = (Button) findViewById(R.id.slot_3);
+        mSlot4 = (Button) findViewById(R.id.slot_4);
+        mSlot5 = (Button) findViewById(R.id.slot_5);
+        mSlot6 = (Button) findViewById(R.id.slot_6);
+        mSlot7 = (Button) findViewById(R.id.slot_7);
+        mSlot8 = (Button) findViewById(R.id.slot_8);
+
+        mSlot1.setOnClickListener(this);
+        mSlot2.setOnClickListener(this);
+        mSlot3.setOnClickListener(this);
+        mSlot4.setOnClickListener(this);
+        mSlot5.setOnClickListener(this);
+        mSlot6.setOnClickListener(this);
+        mSlot7.setOnClickListener(this);
+        mSlot8.setOnClickListener(this);
+
+
+
+
+
+    }
+        public void openMap(){
+            Intent intent = new Intent(this, MapsActivity.class);
+            startActivity(intent);
+
+        }
+
+    @Override
+    public void onClick(View view) {
+        openMap();
     }
 }
+
